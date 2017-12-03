cc0ec3646fde5f31a353b352ca0595e31ac73133
victorshenbr <victorshenbr@yahoo.com>
2017-12-03 17:50:00 -0500

    ridge regression

--- init.py
+++ init.py
@@ -1,15 +1,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import collections
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import GradientBoostingClassifier as gbm
+from scipy.sparse import csr_matrix, hstack
+
 
 pd.options.display.max_rows = 50
 pd.options.display.max_columns = 999
-
-dir = 'C:/gitproject/machineLearning/'
-df = pd.read_table(dir + "train.tsv")
-tests = pd.read_table(dir + "test.tsv")
 
 def splitCats(df):
     df['splited_categories'] = df.apply(lambda row: str(row["category_name"]).split("/"), axis = 1)
@@ -26,21 +27,11 @@ def splitCats(df):
     len(uniqueCat)
 
 # exploration for categories by count, col is the column name
-def plotCol(col):
+def plotCol(df, col):
     val = df[col].value_counts()
     l = range(len(val))
     plt.bar(l, val)
     plt.xticks(l,val.keys(), rotation='vertical')
-
-def vectorization(df):
-    colNameList = df.columns
-    textCol = df['item_description'].apply(lambda x: str(x))
-
-    #tfidf = TfidfVectorizer( stop_words='english')
-    #tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0, stop_words = 'english')
-    #tfs = tfidf.fit_transform(textCol)
-    countvec = CountVectorizer()
-    tfidfVal = countvec.fit_transform(textCol)
 
 #descDf = pd.DataFrame(tfidfVal.toarray(), columns=countvec.get_feature_names())
 def fillMissingVal(df):
@@ -70,24 +61,58 @@ def tfidfTransform(df):
     # transform name to tfidf
     cv = CountVectorizer(min_df = minTermFrequency)
     nameTermFrequency = cv.fit_transform(df['name'])
-
     cvCat = CountVectorizer()
     catTermFrequency = cvCat.fit_transform(df['category_name'])
-
     maxDescriptCount = 50000
     tv = TfidfVectorizer(min_df=maxDescriptCount, ngram_range=(1,3), stop_words='english')
     descriptionFrequency = tv.fit_transform(df['item_description'])
 
+def main():
+    dir = 'C:/gitproject/machineLearning/'
+    df = pd.read_table(dir + "train.tsv")
+    tests = pd.read_table(dir + "test.tsv")
+
+    df['price'] = np.log1p(df['price'])
+    trainRowCount = df.shape[0]
+    trainY = df['price']
+    del df['price']
+    merge: pd.DataFrame = pd.concat([df, tests])
+    submission: pd.DataFrame = tests[['test_id']]
+    del df
+    del tests
+
+    fillMissingVal(merge)
+    reshapeData(merge)
+    colToCat(merge)
+
+    minTermFrequency = 10
+    # transform name to tfidf
+    cv = CountVectorizer(min_df = minTermFrequency)
+    X_nameTermFrequency = cv.fit_transform(merge['name'])
+    cvCat = CountVectorizer()
+    X_catTermFrequency = cvCat.fit_transform(merge['category_name'])
+    maxDescriptCount = 50000
+    tv = TfidfVectorizer(min_df=maxDescriptCount, ngram_range=(1,3), stop_words='english')
+    X_descriptionFrequency = tv.fit_transform(merge['item_description'])
+
+    lb = LabelBinarizer(sparse_output=True)
+    x_brandName = lb.fit_transform(merge['brand_name'])
+
+    # compress sparsed categorical matrix to CSR
+    X_dummies = csr_matrix(pd.get_dummies(merge[['item_condition_id', 'shipping']],sparse=True).values)
+
+    # create train parameters from
+    sparse_merge = hstack((X_dummies, X_nameTermFrequency, X_catTermFrequency, X_descriptionFrequency, x_brandName)).tocsr()
+
+    x_trainDf = sparse_merge[:trainRowCount]
+    x_test = sparse_merge[trainRowCount:]
 
 
-df['price'] = np.log1p(df['price'])
-trainRowCount = df.shape[0]
-trainY = df['price']
-del df['price']
-merge: pd.DataFrame = pd.concat([df, tests])
-submission: pd.DataFrame = tests[['test_id']]
-del df
-del tests
+    model = Ridge(solver="sag", fit_intercept=True, random_state=500)
+    model.fit(x_trainDf, trainY)
+    preds = model.predict(X=x_test)
+    submission['price'] = np.expm1(preds)
+    submission.to_csv("submission_ridge.csv", index=False)
 
-
-
+if __name__ =="__main__":
+    main()
